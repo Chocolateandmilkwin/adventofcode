bfe1538bf2214184bb3fd581b779fa950c09faa2
Chocolateandmilkwin <nicknielsen1995@hotmail.com>
2025-12-02 20:27:12 +0100

    Day one

--- 2025/01/ts/index.ts
+++ 2025/01/ts/index.ts
@@ -1,5 +1,25 @@
 import { warn } from "console";
 import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
 
-let file = await fs.readFile("input.txt", "utf-8");
-warn("File content length:", file.length);
+//Get file
+let filePath = path.join(
+  path.dirname(fileURLToPath(import.meta.url)),
+  "input.txt"
+);
+let file = await fs.readFile(filePath, "utf-8");
+
+//Dial
+let lines = file.split("\n");
+let dial = 50;
+let count = 0;
+for (let line of lines) {
+  let number = Number(line.slice(1));
+  let offset = line[0] === "L" ? -number : number;
+  dial += offset % 100;
+  if (dial < 0) dial += 100;
+  else if (dial >= 100) dial -= 100;
+  if (dial == 0) count++;
+}
+warn("Final dial value:", dial, count);
