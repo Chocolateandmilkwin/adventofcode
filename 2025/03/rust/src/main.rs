8ac76b171523159649ab48453b0b4ccffe3afd65
Chocolateandmilkwin <nicknielsen1995@hotmail.com>
2025-12-05 19:53:29 +0100

    a

--- 2025/03/rust/src/main.rs
+++ 2025/03/rust/src/main.rs
@@ -8,8 +8,9 @@ fn main() {
     //     |  _ <|  _  /| |  | |  | |  |  __|   |  __|| |  | |  _  /| |    |  __|
     //     | |_) | | \ \| |__| |  | |  | |____  | |   | |__| | | \ \| |____| |____
     //     |____/|_|  \_\\____/   |_|  |______| |_|    \____/|_|  \_\\_____|______|
-    let lines = file
-        .split(|num| *num == b'\n')
+    let lines = file.split(|num| *num == b'\n').collect::<Vec<&[u8]>>();
+    let jolts = lines
+        .iter()
         .map(|l| {
             let m = l
                 .iter()
@@ -25,10 +26,41 @@ fn main() {
                 (m.1 as i32) * 10 + (l[i + 1..].iter().map(|b| b - 48).max().unwrap() as i32)
             }
         })
-        .map(|v| {
-            println!("{:?}", v);
-            v
-        })
         .fold(0, |a, b| a + b);
-    println!("Hello, world! {:?} ", lines);
+    println!("Hello, world! {:?} ", jolts);
+
+    let ln = lines
+        .iter()
+        .map(|l| {
+            let mut lc = l
+                .iter()
+                .map(|v| *v - 48)
+                .enumerate()
+                .collect::<Vec<(usize, u8)>>();
+            lc.sort_by(|(_ai, a), (_bi, b)| a.cmp(b));
+            lc
+        })
+        .collect::<Vec<Vec<(usize, u8)>>>();
+
+    println!("Hello, world! {:?} ", ln.last().unwrap());
+
+    fn count(lines: &Vec<Vec<(usize, u8)>>, amount: usize) -> usize {
+        if amount == 0 {
+            return 0;
+        }
+        lines.iter().fold(0, |a, l| {
+            let mut batts = l[(l.len() - amount)..]
+                .iter()
+                .collect::<Vec<&(usize, u8)>>();
+            batts.sort_by(|(ai, _a), (bi, _b)| ai.cmp(bi).reverse());
+            let jolt = batts.iter().enumerate().fold(0, |aa, (bi, (_bii, b))| {
+                aa + ((*b as u64) * 10u64.pow(bi as u32))
+            }) as usize;
+            a + jolt
+        })
+    }
+
+    let count1 = count(&ln, 2);
+
+    println!("Hello, world! {:?} ", count1);
 }
